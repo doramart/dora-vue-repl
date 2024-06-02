b9284a65060552775df99ce513892e4d53d28292
xiaoshen <xiaoshen@isrc.iscas.ac.cn>
2024-06-02 14:03:58 +0800

    vue-repl 代码初始化

---
+++ src/output/Output.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import Preview from './Preview.vue'
+import type { Store } from '../store'
+import { type Ref, computed, inject, ref } from 'vue'
+import type { EditorComponentType, OutputModes } from '../types'
+
+const props = defineProps<{
+  editorComponent: EditorComponentType
+  showCompileOutput?: boolean
+  ssr: boolean
+}>()
+
+const store = inject('store') as Store
+const previewRef = ref<InstanceType<typeof Preview>>()
+const showCompileOutputBar = inject<Ref<boolean>>('compile-output-bar')
+const modes = computed(() =>
+  props.showCompileOutput
+    ? (['preview', 'js', 'css', 'ssr'] as const)
+    : (['preview'] as const),
+)
+
+const mode = ref<OutputModes>(
+  (modes.value as readonly string[]).includes(store.initialOutputMode)
+    ? (store.initialOutputMode as OutputModes)
+    : 'preview',
+)
+
+function reload() {
+  previewRef.value?.reload()
+}
+
+defineExpose({ reload })
+</script>
+
+<template>
+  <div v-if="showCompileOutputBar" class="tab-buttons">
+    <button
+      v-for="m of modes"
+      :key="m"
+      :class="{ active: mode === m }"
+      @click="mode = m"
+    >
+      <span>{{ m }}</span>
+    </button>
+  </div>
+
+  <div class="output-container">
+    <Preview ref="previewRef" :show="mode === 'preview'" :ssr="ssr" />
+    <props.editorComponent
+      v-if="mode !== 'preview'"
+      readonly
+      :filename="store.state.activeFile.filename"
+      :value="store.state.activeFile.compiled[mode]"
+      :mode="mode"
+    />
+  </div>
+</template>
+
+<style scoped>
+.output-container {
+  height: calc(100% - var(--header-height));
+  overflow: hidden;
+  position: relative;
+}
+
+.tab-buttons {
+  box-sizing: border-box;
+  border-bottom: 1px solid var(--border);
+  background-color: var(--bg);
+  height: var(--header-height);
+  overflow: hidden;
+}
+.tab-buttons button {
+  padding: 0;
+  box-sizing: border-box;
+}
+.tab-buttons span {
+  font-size: 13px;
+  font-family: var(--font-code);
+  text-transform: uppercase;
+  color: var(--text-light);
+  display: inline-block;
+  padding: 8px 16px 6px;
+  line-height: 20px;
+}
+button.active {
+  color: var(--color-branding-dark);
+  border-bottom: 3px solid var(--color-branding-dark);
+}
+</style>
